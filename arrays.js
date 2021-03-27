f0bd1a7cc6a1972ce88c63ca14a894bbc0ba0215
Rose Martus <martus.rose@gmail.com>
2021-03-27 16:44:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array = array.shift()
   return array
 }
