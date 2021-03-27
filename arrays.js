6fdd533e450ffe9decfa76f27ab988330257013b
Rose Martus <martus.rose@gmail.com>
2021-03-27 16:32:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
+//chocolateBars
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+//addElementToBeginningOfArray
 function addElementToBeginningOfArray(array,element){
   return [element, ...array]
 }
